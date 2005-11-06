cb9ba0659af41877e0878d6480dc36e9093c86d9
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2005-11-06 11:53:09 +0000

    * dot2home.sh  - 実行属性付加.  - 適当に仕上げてみた. このままでも大体使えるはず.

--- dot2home.sh
+++ dot2home.sh
@@ -1,16 +1,21 @@
-#!/bin/zsh
+#!/bin/sh
 
-DOTDIR=${HOME}/dot.files/
+DOTDIR=~/dot.files
 
-for FILE in ${DOTDIR}/e
-  do ls -lha ${FILE}
+cd ${DOTDIR}
+
+chmod 0710 .fetchmailrc
+
+for FILE in .*
+  do
+    if [ ${FILE} != "." -a ${FILE} != ".." -a ${FILE} != ".svn" ]; then
+	(cd;ln -sfv ${DOTDIR}/${FILE} .)
+    fi
 done
 
-if [ ${DOTDIR} -ef ${PWD} ]; then
-    echo "OK"
-else
-    echo "NG"
-fi
-
-
-
+case `uname` in
+    CYGWIN*)
+	rm -f ~/.wl
+	echo '(load (expand-file-name "~/dot.files/.wl"))' > ~/.wl
+	;;
+esac
