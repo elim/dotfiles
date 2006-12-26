4f9fdda0aed5ccaf372374edeac88c711fc5d18c
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2006-12-26 14:36:24 +0000

    call system specified .screenrc file '.screenrc.specific'

--- dot2home.sh
+++ dot2home.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 # -*- mode: shell-script; coding: utf-8-unix -*-
+# $Id: .screenrc 110 2006-12-20 15:42:18Z takeru $
 
 DOTDIR=~/dot.files
 
 cd ${DOTDIR}
 
+### xsession
 ln -vsf .xsession .xinitrc
 
+### screenrc
+SCREENRC_HOST_SPECIFIC_FILE=.screenrc.$(hostname)
+SCREENRC_SPECIFIC_FILENAME=.screenrc.specific
+
+rm -f ${DOTDIR}/${SCREENRC_SPECIFIC_FILENAME}
+
+if [ -f ${SCREENRC_HOST_SPECIFIC_FILE} ]; then
+    cp -v ${SCREENRC_HOST_SPECIFIC_FILE} ${SCREENRC_SPECIFIC_FILENAME}
+else
+    touch ${SCREENRC_SPECIFIC_FILENAME}
+fi
+    
+### disposition
 for FILE in .*
 do
     case ${FILE} in
@@ -17,3 +32,4 @@ do
 	    ;;
     esac
 done
+
