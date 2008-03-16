8e2f82330824d709424ab5401c2d8216ec361b91
takeru <takeru@66c708bb-4ed7-0310-8515-f78b802efa7e>
2008-03-16 03:27:25 +0000

    .screenrc:   - fixed typo.
    dot2home.sh:
      - fixed path settings.

--- dot2home.sh
+++ dot2home.sh
@@ -4,31 +4,25 @@
 
 # http://blog.hansode.org/archives/51481467.html
 dotdir=$(echo $(cd $(dirname $0) && pwd))
-cd ${dotdir}
-
-### emacs
-rm -f .emacs
-if type emacs &> /dev/null; then 
-  if  emacs --version |grep '\(19\|20\|21\)\.'; then
-    echo '(load (expand-file-name "~/.emacs.d/init.el"))' > .emacs
-  fi
-fi
-
-### xsession
-ln -vsf .xsession .xinitrc
 
 ### screenrc
 screenrc_d=.screenrc.d
-screenrc_local_src=${screenrc_d}/.screenrc.$(hostname)
-screenrc_local_dest=${screenrc_d}/.screenrc.local
+screenrc_local_src=.screenrc.$(hostname)
+screenrc_local_dest=.screenrc.local
 
-rm -f ${dotdir}/${screenrc_local_dest}
+cd ${dotdir}/${screenrc_d}
+rm -f ${screenrc_local_dest}
 
 if [ -f ${screenrc_local_src} ]; then
   ln -sfv  ${screenrc_local_src} ${screenrc_local_dest}
 else
   touch ${screenrc_local_dest}
 fi
+
+cd ${dotdir}
+
+### xsession
+ln -vsf .xsession .xinitrc
 
 ### disposition
 for f in .*
