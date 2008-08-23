e29e6b3c29b63818c5a5d6237bb2549b156c9d90
Takeru Naito <takeru.naito@gmail.com>
2008-08-23 23:40:50 +0900

    hardstatus の設定は別途 Ruby にさせることにした。

--- dot2home.sh
+++ dot2home.sh
@@ -1,23 +1,8 @@
 #!/bin/sh
 # -*- mode: shell-script; coding: utf-8-unix -*-
-# $Id$
 
 # http://blog.hansode.org/archives/51481467.html
 dotdir=$(echo $(cd $(dirname $0) && pwd))
-
-### screenrc
-screenrc_d=.screenrc.d
-screenrc_local_src=.screenrc.$(hostname)
-screenrc_local_dest=.screenrc.local
-
-cd ${dotdir}/${screenrc_d}
-rm -f ${screenrc_local_dest}
-
-if [ -f ${screenrc_local_src} ]; then
-  ln -sfv  ${screenrc_local_src} ${screenrc_local_dest}
-else
-  touch ${screenrc_local_dest}
-fi
 
 cd ${dotdir}
 
@@ -28,7 +13,7 @@ ln -vsf .xsession .xinitrc
 for f in .*
 do
   case ${f} in
-    '.'|'..'|'.svn'|*~)
+    '.'|'..'|'.git'|*~)
       ;;
     *)
       (cd;ln -vsf ${dotdir}/${f} .)
