66d753a74afc692757295a8bfc3b570b27368a17
Takeru Naito <takeru@trinity.coova.org>
2008-09-09 19:04:48 +0900

    Separated GNU screen setting file.

--- .screenrc.d/screenrc.rb
+++ .screenrc.d/screenrc.rb
@@ -1,26 +1,67 @@
 #!/usr/bin/env ruby
+# -*- mode: ruby; coding: utf-8-unix; indent-tabs-mode: nil -*-
+
+############################################################
+## functions
+
+def screen_eval(str, exec = false)
+  if exec
+    exec('screen', '-X', 'eval', str)
+  else
+    system('screen', '-X', 'eval', str)
+  end
+end
+
+def startup?
+  not not %x(screen -ls).match(/\t#{Process.ppid}\./)
+end
+
+
+############################################################
+## variables
 
 session_name = ENV['STY'].gsub(/.+?\.(.+)/, '\1')
-status_format = %Q(hardstatus alwayslastline ) +
-                %Q("%{= KC}[%H:#{session_name}] ) +
-                %Q(%<%{= KW}%-Lw%{= WK}%50>%n%f* %t%{-}%+Lw%<%{= KY} %Y-%m-%d %c")
-
-system("screen", "-X", "eval", status_format)
 
 ############################################################
 ## encoding
 
 case PLATFORM
 when /cygwin/
-  enc = 'sjis'
+  enc = 'SJIS'
 else
-  enc = 'utf8'
+  enc = 'UTF-8'
 end
 
-system("screen", "-X", "eval", "defutf8 #{enc == 'utf8' ? 'on' : 'off'}")
-system("screen", "-X", "eval", "defencoding #{enc}")
-system("screen", "-X", "eval", "encoding #{enc} #{enc}")
+screen_eval "defutf8 #{enc == 'UTF-8' ? 'on' : 'off'}"
+screen_eval "defencoding #{enc}"
+screen_eval "encoding #{enc} #{enc}"
+screen_eval 'cjkwidth on' if enc == 'UTF-8'
 
-%w(daemon rails).each do |session_type|
-  exec('screen', '-X', 'eval', 'zombie QR') if session_name == session_type
+############################################################
+## hardstatus
+
+hardstatus = %Q(hardstatus alwayslastline ) +
+                %Q("%{= KC}[%H:#{session_name}] ) +
+                %Q(%<%{= KW}%-Lw%{= WK}%50>%n%f* %t%{-}%+Lw%<%{= KY} %Y-%m-%d %c")
+
+screen_eval hardstatus
+
+############################################################
+## startup
+
+if startup?
+   case session_name
+    when 'daemon'
+    when 'rails'
+     screen_eval 'screen 4 ./script/server'
+     screen_eval 'screen 3 ./script/console'
+     screen_eval 'select 0'
+   end
+end
+
+############################################################
+## zombie
+case session_name
+when *%w(daemon rails)
+  screen_eval 'zombie QR', :exec
 end
