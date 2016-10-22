343f45882925b5c6ff097c75fe6145ede0a7adbc
Takeru Naito <takeru.naito@gmail.com>
2016-10-22 13:11:45 +0900

    Put the ignore pattern into the $XDG_CONFIG_HOME

--- deploy.sh
+++ deploy.sh
@@ -20,23 +20,45 @@ default_gems() {
   ln -fvs ${dotdir}/rbenv/default-gems ${rbenv_dir}
 }
 
+git_ignore() {
+  local dest src
+  local d f
+
+  local dotdir=$(dirname $(realpath ${0}))
+  local xdg_config_home=${XDG_CONFIG_HOME:-$HOME/.config}
+
+  if [ -d ${xdg_config_home} ]; then
+    cd ${dotdir}/.config
+
+    for d in $(find . -type d); do
+      mkdir -p $(realpath ${xdg_config_home}/${d})
+    done
+
+    for f in $(find . -type f); do
+      src=$(realpath ${dotdir}/.config/${f})
+      dest=$(realpath ${xdg_config_home}/${f})
+
+      ln -fvs ${src} ${dest}
+    done
+  fi
+}
+
 simple_link() {
   local dotdir=$(dirname $(realpath ${0}))
 
-  ln -fvs ${dotdir}/.bashrc           ~
-  ln -fvs ${dotdir}/.cvsrc            ~
-  ln -fvs ${dotdir}/.git.d            ~
-  ln -fvs ${dotdir}/.global-gitignore ~
-  ln -fvs ${dotdir}/.my.cnf           ~
-  ln -fvs ${dotdir}/.sshrc            ~
-  ln -fvs ${dotdir}/.sshrc.d          ~
-  ln -fvs ${dotdir}/.tmux.conf        ~
-  ln -fvs ${dotdir}/.tmux.d           ~
-  ln -fvs ${dotdir}/.vimrc            ~
-  ln -fvs ${dotdir}/.xsession         ~
-  ln -fvs ${dotdir}/.zsh.d            ~
-  ln -fvs ${dotdir}/.zshenv           ~
-  ln -fvs ${dotdir}/.zshrc            ~
+  ln -fvs ${dotdir}/.bashrc    ~
+  ln -fvs ${dotdir}/.cvsrc     ~
+  ln -fvs ${dotdir}/.git.d     ~
+  ln -fvs ${dotdir}/.my.cnf    ~
+  ln -fvs ${dotdir}/.sshrc     ~
+  ln -fvs ${dotdir}/.sshrc.d   ~
+  ln -fvs ${dotdir}/.tmux.conf ~
+  ln -fvs ${dotdir}/.tmux.d    ~
+  ln -fvs ${dotdir}/.vimrc     ~
+  ln -fvs ${dotdir}/.xsession  ~
+  ln -fvs ${dotdir}/.zsh.d     ~
+  ln -fvs ${dotdir}/.zshenv    ~
+  ln -fvs ${dotdir}/.zshrc     ~
 }
 
 # ----------------------------
@@ -45,3 +67,4 @@ simple_link() {
 
 simple_link
 default_gems
+git_ignore
