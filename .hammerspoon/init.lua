961661e614cd8fe8dfcd1b296a87b85be2c0fbd8
Takeru Naito <takeru.naito@gmail.com>
2016-12-19 10:47:25 +0900

    Disable Hammerspoon in the Emacs

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -36,7 +36,7 @@ end
 local function handleGlobalAppEvent(name, event, app)
    if event == hs.application.watcher.activated then
       -- hs.alert.show(name)
-      if name == "iTerm2" then
+      if name == "iTerm2" or name == "Emacs" then
          disableAllHotkeys()
       else
          enableAllHotkeys()
