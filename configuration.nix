513dc4f843e8c838317d7ad8f505e118dafbbee2
Takeru Naito <takeru.naito@gmail.com>
2021-08-30 14:16:22 +0900

    Set wireless interface

--- configuration.nix
+++ configuration.nix
@@ -49,6 +49,7 @@
   # networking.hostName = "nixos"; # Define your hostname.
   networking.networkmanager.enable = true;
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.interfaces = [ "wlp0s20f3" ];
 
   # Set your time zone.
   time.timeZone = "Asia/Tokyo";
