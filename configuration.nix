1c0996cd21a0ee3aacade0387ba03949461e3f20
Takeru Naito <takeru.naito@gmail.com>
2021-08-30 14:23:28 +0900

    Enable touchpad.disableWhileTyping
    However, I'm not too happy with it ...

--- configuration.nix
+++ configuration.nix
@@ -102,7 +102,8 @@
   hardware.pulseaudio.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
+  services.xserver.libinput.touchpad.disableWhileTyping = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   # users.users.jane = {
