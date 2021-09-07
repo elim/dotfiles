91119f90ce2b5715f9e01e67b7f8b8334a0c0ae2
Takeru Naito <takeru.naito@gmail.com>
2021-09-07 11:42:08 +0900

    home-manager: Add GNU Aspell with English dictionaries

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }:
 
-{
+let
+  aspell = pkgs.aspellWithDicts
+    (dicts: with dicts; [ en en-computers en-science ]);
+in {
   home.packages = with pkgs; [
+    aspell
     emacs
   ];
 }
