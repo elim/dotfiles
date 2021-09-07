bbbf4edb2e1cd1ea91785c84e61da7f27fa04039
Takeru Naito <takeru.naito@gmail.com>
2021-09-07 17:21:25 +0900

    home-manager: Can specify nixpkgs-unstable as unstable
    Will also need the following:
    
        nix-channel --update nixpkg-unstable

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -3,6 +3,7 @@
 let
   aspell = pkgs.aspellWithDicts
     (dicts: with dicts; [ en en-computers en-science ]);
+  unstable = import <nixpkgs-unstable> {};
 in {
   home.packages = with pkgs; [
     aspell
