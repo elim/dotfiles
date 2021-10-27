b47e4857dc13903b886130c2f209d6a5278131a6
Takeru Naito <takeru.naito@gmail.com>
2021-10-27 11:51:29 +0900

    Aggregate X settings into one block

--- configuration.nix
+++ configuration.nix
@@ -73,19 +73,28 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
 
+    # Enable the GNOME Desktop Environment.
+    displayManager.gdm = {
+      enable = true;
+      wayland = false;
+    };
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = false;
-  services.xserver.desktopManager.gnome.enable = true;
-  
+    desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-  services.xserver.xkbOptions = "ctrl:nocaps";
+    # Configure keymap in X11
+    xkbOptions = "ctrl:nocaps";
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    libinput = {
+      enable = true;
+      touchpad = {
+        disableWhileTyping = true;
+      };
+    };
+  };
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
@@ -102,9 +111,6 @@
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-  services.xserver.libinput.touchpad.disableWhileTyping = true;
 
   services.thermald.enable = true;
 
