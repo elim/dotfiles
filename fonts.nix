d9e5126533f7cdbc2cbc16f5d71a3b08b12182ef
Takeru Naito <takeru.naito@gmail.com>
2023-03-09 15:38:44 +0900

    Use Fira Code and BIZ UD on apps using fontconfig

--- fonts.nix
+++ fonts.nix
@@ -17,6 +17,11 @@ in
         ];
       })
 
+      fira-code
+      fira-code-symbols
+      fira-mono
+      libertine
+
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
@@ -26,18 +31,15 @@ in
     fontconfig = {
       defaultFonts = {
         serif = [
+          "Linux Libertine"
           "BIZ UDMincho"
-          "Noto Serif"
-          "Noto Serif CJK JP"
         ];
         sansSerif = [
-          "Cica"
+          "Fira Code"
           "BIZ UDGothic"
-          "Noto Sans"
-          "Noto Sans CJK JP"
         ];
         monospace = [
-          "Cica"
+          "Fira Code"
           "Noto Sans Mono"
           "Noto Sans Mono CJK JP"
         ];
