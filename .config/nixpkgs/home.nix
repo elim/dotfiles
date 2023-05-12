8f5ddb71f5d5bd38f92e15de69cc06e540a0628d
Takeru Naito <takeru.naito@gmail.com>
2023-05-12 14:35:13 +0900

    GTK: Set themes

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -74,6 +74,51 @@ in
     zoom-us
   ]);
 
+  gtk = {
+    enable = true;
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    theme = {
+      name = "palenight";
+      package = pkgs.palenight-theme;
+    };
+
+    cursorTheme = {
+      name = "Numix-Cursor";
+      package = pkgs.numix-cursor-theme;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+  home.sessionVariables.GTK_THEME = "palenight";
+
+  dconf = {
+    settings = {
+      "org/gnome/desktop/interface" = {
+        gtk-theme = "${config.gtk.theme.name}";
+        cursor-theme = "${config.gtk.cursorTheme.name}";
+      };
+      "org/gnome/desktop/wm/preferences" = {
+        theme = "${config.gtk.theme.name}";
+      };
+    };
+  };
+
   services.dropbox = {
     enable = true;
   };
