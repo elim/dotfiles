04ac3d85a4c2846fa410e784b3a35df6c8c24a30
Takeru Naito <takeru.naito@gmail.com>
2024-02-21 13:11:55 +0900

    Specify package repository version to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
