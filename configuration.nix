d1574300b2b5af129d1d20e5081606859379dbe0
Takeru Naito <takeru.naito@gmail.com>
2024-02-22 11:12:48 +0900

    Adopt PipeWire instead of PulseAudio

--- configuration.nix
+++ configuration.nix
@@ -153,9 +153,16 @@ in
   };
 
   # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
 
   services.thermald.enable = true;
 
