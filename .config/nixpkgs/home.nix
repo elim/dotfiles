4793e268a96516919547bec585ac984c1ca65887
Takeru Naito <takeru.naito@gmail.com>
2024-02-29 17:20:11 +0900

    home-manager: Use nixpkgs' Emacs 29 with Tree-Sitter

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -5,7 +5,14 @@ let
 
   aspell = pkgs.aspellWithDicts
     (dicts: with dicts; [ en en-computers en-science ]);
-  emacs = pkgs.emacs-unstable;
+
+  # Based on "Integrating Tree-Sitter with Emacs29 in nix-darwin"
+  # https://nohzafk.github.io/posts/2023-12-18-nix-emacs-treesit-grammars/
+  buildEmacs = (pkgs.emacsPackagesFor pkgs.emacs29).emacsWithPackages;
+  treesitGrammars =
+    (pkgs.emacsPackagesFor pkgs.emacs29).treesit-grammars.with-all-grammars;
+  emacs = buildEmacs (epkgs: with epkgs; [ vterm treesitGrammars ]);
+
   handbrake = pkgs.handbrake.override { useFdk = true; };
 in
 {
