8cf0ab211cc9c416bd0063ffc53deaab1059e9e3
Takeru Naito <takeru.naito@gmail.com>
2024-08-08 23:19:21 +0900

    home-manager: Add my own forked version's whichpr

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -23,6 +23,39 @@ let
       hash = "sha256-/mow+X1U4Cl2jy3fSSNpnEFs28Om6W2z4tVxbH25ajQ=";
     };
   });
+
+  whichpr = (
+    pkgs.buildGoModule rec {
+      pname = "whichpr";
+      version = "v1.0.0";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "elim";
+        repo = "whichpr";
+        rev = "ca06bf59ec2a83113c17ce9a0c8c2940808e5efe";
+        hash = "sha256-9lygIUitfP8itNEUyXhCaULPLExgmohEYnDSnoJugDQ=";
+      };
+
+      vendorHash = "sha256-zx2jVFP0MWCpymhktSTBKEAkOzuQzi12959RNQ7WknA=";
+
+      buildInputs = [ pkgs.git ];
+
+      preBuild = ''
+        export HOME=$(mktemp -d)
+        export PATH=${pkgs.git}/bin:$PATH
+        git config --global user.name "Test User"
+        git config --global user.email test.user@example.com
+      '';
+
+      buildPhase = ''
+        go build
+      '';
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp whichpr $out/bin
+      '';
+    });
 in
 {
 
@@ -76,6 +109,7 @@ in
     xkeysnail
     xorg.xhost
     xsel
+    whichpr
   ] ++ (with unstable; [
     avidemux
     azure-cli
