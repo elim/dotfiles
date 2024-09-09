3fb2bd9f88ab825fa7c2167fbbf55f370923202f
Takeru Naito <takeru.naito@gmail.com>
2024-09-09 14:25:39 +0900

    nix: Adopt nixfmt for consistent formatting
    Standardize Nix files by adopting the nixfmt formatter. All existing
    files have been reformatted to comply with this style guide, improving
    code readability and consistency.
    
    No code logic has been altered, only formatting.

--- flake.nix
+++ flake.nix
@@ -1,16 +1,34 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = inputs: {
-    nixosConfigurations = {
-      myNixOS = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-        ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      treefmt-nix,
+      ...
+    }:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+    in
+    {
+      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+
+      checks = eachSystem (pkgs: {
+        formatting = treefmtEval.${pkgs.system}.config.build.check self;
+
+      });
+
+      nixosConfigurations = {
+        myNixOS = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+        };
       };
     };
-  };
 }
