b7b3021a600ba78061802e68ef4c459655679880
Takeru Naito <takeru.naito@gmail.com>
2024-09-12 15:34:19 +0900

    ollma: Adopt Ollama from nixos-unstable
    Adopting the Ollama package from nixos-unstable because the version in
    nixos-24.05 is too old to support Llama 3.1.
    
    However, note that Ollama does not utilize CUDA in the current
    configuration. I plan to address this issue later.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
@@ -8,25 +9,34 @@
     {
       self,
       nixpkgs,
+      nixpkgs-unstable,
       systems,
       treefmt-nix,
       ...
     }:
     let
+      system = "x86_64-linux";
       eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
       treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
     in
     {
       formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
 
       checks = eachSystem (pkgs: {
         formatting = treefmtEval.${pkgs.system}.config.build.check self;
-
       });
 
       nixosConfigurations = {
         myNixOS = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
+          inherit system;
+          specialArgs = {
+            pkgs-unstable = import nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+              config.cudaSupport = true;
+            };
+          };
           modules = [ ./configuration.nix ];
         };
       };
