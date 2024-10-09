de92472acf388cd826c8c2a008464f0adcfe6fbd
Takeru Naito <takeru.naito@gmail.com>
2024-10-09 11:47:03 +0900

    flake: Refer to nixpkgs-unstable from treefmt
    Updated the flake to make the treefmt-nix use the nixpkgs-unstable via
    the "follows" attribute. This change reduces duplication by
    eliminating one reference to the nixpkgs.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
   };
 
   outputs =
