409f89dca53ec5b6894acf377e29b9a2bd97e0f2
Takeru Naito <takeru.naito@gmail.com>
2024-11-11 22:52:47 +0900

    home-manager: Manage existing dotfiles
    Manage existing dotfiles with home-manager.
    
    This commit removes the existing deployment script and migrates the
    management of dotfiles to home-manager.
    
    The configurations for anyenv, rbenv, nodenv, and pyenv are also
    removed as they will be replaced with Nix expressions in the
    future. While the ideal approach is to manage all environments through
    Nix, it may be necessary to use rbenv from nixpkgs due to unforeseen
    circumstances.

---
+++ modules/legacies/default.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }:
+let
+  symlink = config.lib.file.mkOutOfStoreSymlink;
+
+  legacies = [
+    ".bashrc"
+    ".config/fcitx/skk/rule"
+    ".config/git/bin/diff-pager"
+    ".config/git/config"
+    ".config/git/ignore"
+    ".config/libskk/rules/MyRule/keymap/default.json"
+    ".config/libskk/rules/MyRule/keymap/hankaku-katakana.json"
+    ".config/libskk/rules/MyRule/keymap/hiragana.json"
+    ".config/libskk/rules/MyRule/keymap/katakana.json"
+    ".config/libskk/rules/MyRule/keymap/latin.json"
+    ".config/libskk/rules/MyRule/keymap/wide-latin.json"
+    ".config/libskk/rules/MyRule/metadata.json"
+    ".config/libskk/rules/MyRule/rom-kana/default.json"
+    ".config/nixpkgs/config.nix"
+    ".config/xkeysnail/config.py"
+    ".config/zsh/.zshrc"
+    ".config/zsh/anyframe-functions/widgets/anyframe-widget-tab-ghq-repository"
+    ".config/zsh/config/alias"
+    ".config/zsh/config/chpwd"
+    ".config/zsh/config/completion"
+    ".config/zsh/config/history"
+    ".config/zsh/config/keybind"
+    ".config/zsh/config/options"
+    ".config/zsh/config/prompt"
+    ".config/zsh/functions/figma-url-cleaner"
+    ".config/zsh/functions/github-markdown-link-reformatter"
+    ".config/zsh/functions/set-copy-paste-command"
+    ".config/zsh/functions/tab"
+    ".config/zsh/functions/tab.js"
+    ".config/zsh/functions/title"
+    ".config/zsh/functions/zman"
+    ".config/zsh/snippets/keychain"
+    ".config/zsh/snippets/tmux"
+    ".local/bin/choosy.sh"
+    ".local/share/applications/choosy.sh.desktop"
+    ".shell.d/alias"
+    ".shell.d/environment"
+    ".shell.d/lscolors"
+    ".shell.d/ulimit"
+    ".tmux.conf"
+    ".tmux.d/linux.conf"
+    ".tmux.d/macos.conf"
+    ".vimrc"
+    ".zshenv"
+  ];
+in
+{
+  home.file = builtins.listToAttrs (
+    map (path: {
+      name = path;
+      value = {
+        source = symlink "${config.home.homeDirectory}/dotfiles/modules/legacies/${path}";
+      };
+    }) legacies
+  );
+}
