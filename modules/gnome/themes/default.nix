b282896569587ff93bb09512f945950c52bd4cfd
Takeru Naito <takeru.naito@gmail.com>
2024-11-17 20:45:51 +0900

    home-manager: Decouple Gnome Themes configuration

---
+++ modules/gnome/themes/default.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+{
+  gtk = {
+    enable = true;
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    theme = {
+      name = "palenight";
+      package = pkgs.palenight-theme;
+    };
+
+    cursorTheme = {
+      name = "Numix-Cursor";
+      package = pkgs.numix-cursor-theme;
+    };
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+
+  home.sessionVariables.GTK_THEME = "palenight";
+
+  dconf = {
+    settings = {
+      "org/gnome/desktop/interface" = {
+        gtk-theme = "${config.gtk.theme.name}";
+        cursor-size = 96;
+        cursor-theme = "${config.gtk.cursorTheme.name}";
+      };
+      "org/gnome/desktop/wm/preferences" = {
+        theme = "${config.gtk.theme.name}";
+      };
+    };
+  };
+}
