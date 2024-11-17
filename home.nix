07b714f224494e49eb198f4da813e224a8746cad
Takeru Naito <takeru.naito@gmail.com>
2024-11-17 15:33:41 +0900

    home-manager: Decouple package configuration

--- home.nix
+++ home.nix
@@ -4,16 +4,6 @@
   pinned-pkgs,
   ...
 }:
-
-let
-  albert = import ./modules/packages/albert { inherit pkgs; };
-  aspell = import ./modules/packages/aspell { inherit pkgs; };
-  azure-cli = pinned-pkgs.azure-cli.azure-cli;
-  emacs = import ./modules/packages/emacs { inherit pkgs; };
-  handbrake = pinned-pkgs.ffmpeg.handbrake;
-  ruby = import ./modules/packages/ruby { inherit pkgs; };
-  whichpr = import ./modules/packages/whichpr { inherit pkgs; };
-in
 {
   home = rec {
     stateVersion = "24.11";
@@ -25,77 +15,6 @@ in
   imports = [
     ./modules
   ];
-
-  home.packages =
-    with pkgs;
-    [
-      albert
-      aspell
-      atomicparsley
-      avidemux
-      azure-cli
-      brave
-      colordiff
-      dash
-      delta
-      docker-compose
-      emacs
-      exiftool
-      fd
-      fdupes
-      ffmpeg
-      firefox
-      ghq
-      gimp
-      github-cli
-      gnomeExtensions.appindicator
-      handbrake
-      htop
-      imagemagick
-      k9s
-      keychain
-      kubectl
-      kubelogin
-      libreoffice
-      nkf
-      nodePackages.sql-formatter
-      peco
-      pgformatter
-      ripgrep
-      ruby
-      shellcheck
-      skktools
-      slack
-      stern
-      thunderbird
-      tmux
-      trurl
-      unar
-      unzip
-      whichpr
-      xkeysnail
-      xorg.xhost
-      xsel
-      yq
-      zenity
-      zoom-us
-    ]
-    ++ [ pkgs.file ]
-    ++ (
-      with skkDictionaries;
-      map (pkg: pkg.override { useUtf8 = true; }) [
-        l
-        jinmei
-        fullname
-        geo
-        okinawa
-        china_taiwan
-        station
-        propernoun
-        itaiji
-        zipcode
-      ]
-    );
 
   gtk = {
     enable = true;
--- modules/default.nix
+++ modules/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./legacies
+    ./packages
   ];
 }
