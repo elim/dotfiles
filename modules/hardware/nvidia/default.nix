7a8323e40b9a95179ed2ad9a6647038343f7748e
Takeru Naito <takeru.naito@gmail.com>
2024-12-02 21:09:42 +0900

    nix: Workaround broken nvidia driver
    Use config.boot.kernelPackages.nvidiaPackages.beta to
    temporarily fix the broken nvidia driver in NixOS. This
    allows the system to build with Kernel 6.12.1 and
    nvidia, and basic functionality has been confirmed.
    
    This solution was found in:
    https://github.com/NixOS/nixpkgs/issues/353990#issuecomment-2467063091
    
    Despite this workaround, nautilus still crashes on startup.

--- modules/hardware/nvidia/default.nix
+++ modules/hardware/nvidia/default.nix
@@ -3,15 +3,10 @@
   hardware.nvidia = {
     modesetting.enable = true;
 
-    # ref: https://discourse.nixos.org/t/49266/18
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "555.58.02";
-      sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
-      sha256_aarch64 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
-      openSha256 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
-      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-      persistencedSha256 = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
-    };
+    open = true;
+
+    # https://github.com/NixOS/nixpkgs/issues/353990#issuecomment-2467063091
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
 
     prime = {
       sync.enable = true;
