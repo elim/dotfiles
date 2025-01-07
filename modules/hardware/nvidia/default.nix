2e79b8d13ced38bfababaa1833ca4c7c26a91439
Takeru Naito <takeru.naito@gmail.com>
2025-01-07 17:50:54 +0900

    nix: Use "stable" nvidia driver
    It works fine with Linux kernel 6.12.8.

--- modules/hardware/nvidia/default.nix
+++ modules/hardware/nvidia/default.nix
@@ -5,9 +5,6 @@
 
     open = true;
 
-    # https://github.com/NixOS/nixpkgs/issues/353990#issuecomment-2467063091
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-
     prime = {
       sync.enable = true;
       intelBusId = "PCI:0:2:0";
