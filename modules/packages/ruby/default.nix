697bd2d792fcdd3c731cffa49fc1f171eb062541
Takeru Naito <takeru.naito@gmail.com>
2025-01-09 10:34:55 +0900

    ruby: Upgrade to v3.4.1

--- modules/packages/ruby/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }:
-pkgs.ruby_3_3.overrideAttrs (oldAttrs: rec {
-  version = "3.3.6";
-  src = pkgs.fetchurl {
-    url = "https://cache.ruby-lang.org/pub/ruby/3.3/ruby-${version}.tar.gz";
-    hash = "sha256-jcSP/68nD4bxAZBT8o5R5NpMzjKjZ2CgYDqa7mfX/Y0=";
-  };
-})
