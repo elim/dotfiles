377e267bb66a12e7c8725ab85cbb2ddb18b47ccd
Takeru Naito <takeru.naito@gmail.com>
2025-01-14 16:26:53 +0900

    azure-cli: Use nixos-unstable package
    Unpin azure-cli revision now that it works as expected
    in nixos-unstable.

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -3,7 +3,7 @@
 let
   albert = import ./albert { inherit pkgs; };
   aspell = import ./aspell { inherit pkgs; };
-  azure-cli = pinned-pkgs.azure-cli.azure-cli.override { withImmutableConfig = false; };
+  azure-cli = pkgs.azure-cli.override { withImmutableConfig = false; };
   e = import ./e { inherit pkgs emacs; };
   emacs = import ./emacs { inherit pkgs; };
   handbrake = pinned-pkgs.ffmpeg.handbrake;
