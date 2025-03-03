2d680b866648953400416b2af1e8c23c8ac34c0d
Takeru Naito <takeru.naito@gmail.com>
2025-03-03 11:18:33 +0900

    zsh: Load mollifier/cd-gitroot via home-manager's Zsh module

--- modules/programs/zsh/default.nix
+++ modules/programs/zsh/default.nix
@@ -33,5 +33,17 @@
       builtins.readFile ./zshrc.legacy
       + builtins.readFile ./snippets/tmux
       + builtins.readFile ./snippets/keychain;
+
+    plugins = [
+      {
+        name = "cd-gitroot";
+        src = pkgs.fetchFromGitHub {
+          owner = "mollifier";
+          repo = "cd-gitroot";
+          rev = "66f6ba7549b9973eb57bfbc188e29d2f73bf31bb";
+          hash = "sha256-pLdF8wbkA9mPI5cg8VPYAW7i3cWNJX3+lfAZ5cZPUgE=";
+        };
+      }
+    ];
   };
 }
