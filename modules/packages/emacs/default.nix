e206887ef416551aa7e757c5e18d23ddcb7a1dd8
Takeru Naito <takeru.naito@gmail.com>
2025-04-01 22:53:47 +0900

    emacs: Switch to pgtk build for Wayland
    Migrated to the pgtk (Pure GTK) build of Emacs after an
    unintentional system shift to Wayland. Taking this opportunity
    to try the pgtk version for better native integration.
    
    Set `GTK_IM_MODULE=gtk-im-context-simple` on Emacs startup.
    This disables Wayland's native input method within Emacs
    to prevent key input conflicts with DDSKK.

--- modules/packages/emacs/default.nix
+++ modules/packages/emacs/default.nix
@@ -2,7 +2,8 @@
 # https://nohzafk.github.io/posts/2023-12-18-nix-emacs-treesit-grammars/
 { pkgs }:
 let
-  buildEmacs = (pkgs.emacsPackagesFor pkgs.emacs30).emacsWithPackages;
-  treesitGrammars = (pkgs.emacsPackagesFor pkgs.emacs30).treesit-grammars.with-all-grammars;
+  emacsPackageSet = pkgs.emacsPackagesFor pkgs.emacs30-pgtk;
+  buildEmacs = emacsPackageSet.emacsWithPackages;
+  treesitGrammars = emacsPackageSet.treesit-grammars.with-all-grammars;
 in
-buildEmacs (epkgs: with epkgs; [ treesitGrammars ])
+buildEmacs (_: [ treesitGrammars ])
