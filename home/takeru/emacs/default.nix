f723fdbcad9951c09fa114528fd95c835f0a9422
Takeru Naito <takeru.naito@gmail.com>
2025-06-12 13:25:40 +0900

    emacs: Localize overlay for dependent packages
    Relocate the custom Emacs definition from a global overlay into the
    `emacs` home-manager module. The definition for the `e` package is
    also moved here from the top-level `packages.nix`.
    
    This change allows `e` to be built with the correct, customized Emacs
    instance via a local overlay, resolving a dependency issue without
    affecting the global package set.

--- home/takeru/emacs/default.nix
+++ home/takeru/emacs/default.nix
@@ -1,5 +1,27 @@
 { pkgs, ... }:
 
+let
+  emacs = pkgs.emacsWithPackagesFromUsePackage {
+    config = builtins.toFile "empty.el" "";
+    package = pkgs.emacs-unstable-pgtk;
+    alwaysEnsure = true;
+    alwaysTangle = true;
+    extraEmacsPackages = epkgs: [
+      epkgs.elisp-slime-nav
+      epkgs.treesit-grammars.with-all-grammars
+    ];
+  };
+
+  overlay =
+    final: prev:
+    prev
+    // {
+      inherit emacs;
+    };
+
+  e = pkgs.callPackage ../../../pkgs/e { pkgs = (pkgs.extend overlay); };
+in
+
 {
   home = {
     file = {
@@ -7,6 +29,14 @@
       ".emacs.d/early-init.el".source = ./early-init.el;
     };
 
-    packages = [ pkgs.emacs ];
+    packages = [
+      emacs
+      e
+    ];
+
+    sessionVariables = {
+      EDITOR = "${e}/bin/e";
+      GIT_EDITOR = "${emacs}/bin/emacsclient";
+    };
   };
 }
