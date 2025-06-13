91413ed3aa8df90a1afe6a48ea9208283ad31bbc
Takeru Naito <takeru.naito@gmail.com>
2025-06-13 16:21:05 +0900

    emacs: Fix indentation of skk's leaf

--- home/takeru/emacs/init.el
+++ home/takeru/emacs/init.el
@@ -689,37 +689,37 @@ Google(with automatic language detection)."
     (let
         ((xdg-based-skk-user-directory
           (expand-file-name "ddskk" (getenv "XDG_CONFIG_HOME"))))
-    (leaf skk
-      :bind* (("C-x C-j" . skk-mode)
-              ("C-x t" . nil)
-              ("C-x j" . nil))
-      :custom ((default-input-method . "japanese-skk")
-               (skk-user-directory . xdg-based-skk-user-directory)
-               (skk-jisyo-code . 'utf-8)
-               (skk-count-private-jisyo-candidates-exactly . t)
-               (skk-share-private-jisyo . t)
-               (skk-server-host . "localhost")
-               (skk-server-portnum . 1178)
-               (skk-japanese-message-and-error . t)
-               (skk-kutouten-type . 'jp)
-               (skk-show-annotation . t)
-               (skk-henkan-strict-okuri-precedence . t)
-               (skk-check-okurigana-on-touroku . 'auto)
-               (skk-isearch-start-mode . 'latin)
-               (skk-search-sagyo-henkaku . t))
-      :config
-      (condition-case nil
-          (skk-server-version)
-        (error
-         (let
-             ((dic-file "/usr/share/skk/SKK-JISYO.L"))
-           (and (file-exists-p dic-file)
-                (set-variable 'skk-jisyo-code nil)
-                (set-variable 'skk-large-jisyo dic-file)))))
-      (let
-          ((skk-auto-save-jisyo-interval 6))
-        (run-with-idle-timer skk-auto-save-jisyo-interval t
-                             #'(lambda () (skk-save-jisyo +1))))))
+      (leaf skk
+        :bind* (("C-x C-j" . skk-mode)
+                ("C-x t" . nil)
+                ("C-x j" . nil))
+        :custom ((default-input-method . "japanese-skk")
+                 (skk-user-directory . xdg-based-skk-user-directory)
+                 (skk-jisyo-code . 'utf-8)
+                 (skk-count-private-jisyo-candidates-exactly . t)
+                 (skk-share-private-jisyo . t)
+                 (skk-server-host . "localhost")
+                 (skk-server-portnum . 1178)
+                 (skk-japanese-message-and-error . t)
+                 (skk-kutouten-type . 'jp)
+                 (skk-show-annotation . t)
+                 (skk-henkan-strict-okuri-precedence . t)
+                 (skk-check-okurigana-on-touroku . 'auto)
+                 (skk-isearch-start-mode . 'latin)
+                 (skk-search-sagyo-henkaku . t))
+        :config
+        (condition-case nil
+            (skk-server-version)
+          (error
+           (let
+               ((dic-file "/usr/share/skk/SKK-JISYO.L"))
+             (and (file-exists-p dic-file)
+                  (set-variable 'skk-jisyo-code nil)
+                  (set-variable 'skk-large-jisyo dic-file)))))
+        (let
+            ((skk-auto-save-jisyo-interval 6))
+          (run-with-idle-timer skk-auto-save-jisyo-interval t
+                               #'(lambda () (skk-save-jisyo +1))))))
     (leaf ddskk-posframe
       :doc "Show Henkan tooltip for ddskk via posframe"
       :after skk
