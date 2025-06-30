e54dab4119a36f7f1f93d06fd2b2a234e1d3e3a3
Takeru Naito <takeru.naito@gmail.com>
2025-06-30 20:11:36 +0900

    feat(nix): lay the groundwork for cross-platform support
    The primary goal of this commit is to prepare the flake for managing
    configurations across multiple platforms, with an eye toward macOS. To
    this end, the hardcoded `system` has been replaced with a more flexible,
    function-based approach.
    
    As a side effect of centralizing the nixpkgs config, `cudaSupport` is
    now also enabled for the home-manager environment. This was not the
    original intent, but I'm not opposed to it.
    
    I suppose this might increase compile times, but I'm curious if it
    could speed up applications like GIMP 3 in return.

--- flake.nix
+++ flake.nix
@@ -40,62 +40,97 @@
     }:
 
     let
-      system = "x86_64-linux";
       supportedSystems = nixpkgs.lib.systems.flakeExposed;
-      eachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
-      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
 
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-        overlays = [
-          emacs-overlay.overlay
-        ];
+      config = {
+        allowUnfree = true;
+        cudaSupport = true;
       };
 
-      pkgs-stable = import nixpkgs-stable {
-        inherit system;
-        config.allowUnfree = true;
+      overlays = [ emacs-overlay.overlay ];
+
+      makePkgs =
+        system:
+        import nixpkgs {
+          inherit system;
+          inherit config;
+          inherit overlays;
+        };
+
+      makeStablePkgs =
+        system:
+        import nixpkgs-stable {
+          inherit system;
+          inherit config;
+        };
+
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = makePkgs system;
+            pkgs-stable = makeStablePkgs system;
+          }
+        );
+
+      commonNixSettings = {
+        nix = {
+          channel.enable = false;
+          settings = {
+            auto-optimise-store = true;
+            experimental-features = [
+              "nix-command"
+              "flakes"
+            ];
+          };
+          gc = {
+            automatic = true;
+            dates = "weekly";
+            options = "--delete-older-than 30d";
+          };
+        };
       };
+      treefmtEval = eachSystem ({ system, pkgs, ... }: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
     in
     {
-      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+      formatter = eachSystem ({ system, pkgs, ... }: treefmtEval.${system}.config.build.wrapper);
 
-      checks = eachSystem (pkgs: {
-        formatting = treefmtEval.${pkgs.system}.config.build.check self;
-      });
+      checks = eachSystem (
+        { system, pkgs, ... }:
+        {
+          formatting = treefmtEval.${system}.config.build.check self;
+        }
+      );
 
       nixosConfigurations = {
         obsidian = nixpkgs.lib.nixosSystem {
-          inherit system;
+          system = "x86_64-linux";
           modules = [
+            commonNixSettings
             {
               nixpkgs = {
-                config = {
-                  allowUnfree = true;
-                  cudaSupport = true;
-                };
-                overlays = [ ];
+                inherit config;
+                inherit overlays;
               };
             }
             ./hosts/obsidian
           ];
-
-          specialArgs = {
-            inherit inputs;
-          };
+          specialArgs = { inherit inputs; };
         };
       };
 
       homeConfigurations = {
         "takeru@obsidian" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
+          pkgs = makePkgs "x86_64-linux";
           modules = [
             xremap-flake.homeManagerModules.default
             ./home/takeru/obsidian.nix
           ];
           extraSpecialArgs = {
-            inherit pkgs-stable;
+            pkgs-stable = makeStablePkgs "x86_64-linux";
+            dotfiles = self;
           };
         };
       };
