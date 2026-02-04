18dc255f00a25c777a98a06b3992fd410aa4ff6c
Takeru Naito <takeru.naito@gmail.com>
2026-02-04 19:20:42 +0900

    feat(cli): add Nord color theme for eza
    Add a custom Nord theme for eza using the official Nord color palette.
    
    Changes:
    - Reorganize eza configuration as eza/default.nix
    - Create themes/nord.nix as separate theme definition
    - Configure programs.eza module with Nord theme via import
    - Enable icons and git integration via home-manager
    - Theme is generated as $XDG_CONFIG_HOME/eza/theme.yml at build time
    
    Theme design:
    The theme uses Nord's carefully curated Arctic color palette:
    - Polar Night (Nord0-3): Muted elements, punctuation, backgrounds
    - Snow Storm (Nord4-6): Normal text, headers, documents
    - Frost (Nord7-10): Directories, symlinks, users, images
    - Aurora (Nord11-15): Git status, warnings, executables, special files
    
    Color mapping philosophy:
    - Directories: Nord9 (#81a1c1, blue) for clear navigation
    - Executables: Nord14 (#a3be8c, green) for actionable items
    - Symlinks: Nord8 (#88c0d0, bright cyan) for reference indicators
    - Modified files: Nord15 (#b48ead, purple) for change awareness
    - New files: Nord14 (#a3be8c, green) for additions
    - Deleted files: Nord11 (#bf616a, red) for removals
    - Warnings: Nord12 (#d08770, orange) for attention
    - Ignored/muted: Nord3 (#4c566a, dark gray) for de-emphasis
    
    Implementation:
    The theme is defined as a Nix attribute set in a separate file
    (themes/nord.nix) and imported into default.nix. This separation
    allows for easier theme management and potential addition of other
    themes in the future. Home-manager's programs.eza.theme option
    accepts attribute sets and automatically generates the theme.yml file.
    
    References:
    - Nord color palette: https://www.nordtheme.com/
    - eza custom themes: https://github.com/eza-community/eza#custom-themes
    - programs.eza.theme: https://mynixos.com/home-manager/option/programs.eza.theme

---
+++ home/takeru/features/cli/eza/themes/nord.nix
@@ -0,0 +1,275 @@
+{
+  colourful = true;
+
+  filekinds = {
+    normal = {
+      foreground = "#d8dee9";
+    };
+    directory = {
+      foreground = "#81a1c1";
+    };
+    symlink = {
+      foreground = "#88c0d0";
+    };
+    pipe = {
+      foreground = "#4c566a";
+    };
+    block_device = {
+      foreground = "#ebcb8b";
+    };
+    char_device = {
+      foreground = "#ebcb8b";
+    };
+    socket = {
+      foreground = "#4c566a";
+    };
+    special = {
+      foreground = "#b48ead";
+    };
+    executable = {
+      foreground = "#a3be8c";
+    };
+    mount_point = {
+      foreground = "#8fbcbb";
+    };
+  };
+
+  perms = {
+    user_read = {
+      foreground = "#88c0d0";
+    };
+    user_write = {
+      foreground = "#b48ead";
+    };
+    user_execute_file = {
+      foreground = "#a3be8c";
+    };
+    user_execute_other = {
+      foreground = "#a3be8c";
+    };
+    group_read = {
+      foreground = "#88c0d0";
+    };
+    group_write = {
+      foreground = "#d08770";
+    };
+    group_execute = {
+      foreground = "#a3be8c";
+    };
+    other_read = {
+      foreground = "#88c0d0";
+    };
+    other_write = {
+      foreground = "#bf616a";
+    };
+    other_execute = {
+      foreground = "#a3be8c";
+    };
+    special_user_file = {
+      foreground = "#bf616a";
+    };
+    special_other = {
+      foreground = "#bf616a";
+    };
+    attribute = {
+      foreground = "#4c566a";
+    };
+  };
+
+  size = {
+    major = {
+      foreground = "#88c0d0";
+    };
+    minor = {
+      foreground = "#b48ead";
+    };
+    number_byte = {
+      foreground = "#d8dee9";
+    };
+    number_kilo = {
+      foreground = "#8fbcbb";
+    };
+    number_mega = {
+      foreground = "#88c0d0";
+    };
+    number_giga = {
+      foreground = "#d08770";
+    };
+    number_huge = {
+      foreground = "#bf616a";
+    };
+    unit_byte = {
+      foreground = "#d8dee9";
+    };
+    unit_kilo = {
+      foreground = "#8fbcbb";
+    };
+    unit_mega = {
+      foreground = "#88c0d0";
+    };
+    unit_giga = {
+      foreground = "#d08770";
+    };
+    unit_huge = {
+      foreground = "#bf616a";
+    };
+  };
+
+  users = {
+    user_you = {
+      foreground = "#5e81ac";
+    };
+    user_root = {
+      foreground = "#b48ead";
+    };
+    user_other = {
+      foreground = "#88c0d0";
+    };
+    group_yours = {
+      foreground = "#8fbcbb";
+    };
+    group_root = {
+      foreground = "#b48ead";
+    };
+    group_other = {
+      foreground = "#d8dee9";
+    };
+  };
+
+  links = {
+    normal = {
+      foreground = "#8fbcbb";
+    };
+    multi_link_file = {
+      foreground = "#88c0d0";
+    };
+  };
+
+  git = {
+    new = {
+      foreground = "#a3be8c";
+    };
+    modified = {
+      foreground = "#b48ead";
+    };
+    deleted = {
+      foreground = "#bf616a";
+    };
+    renamed = {
+      foreground = "#88c0d0";
+    };
+    typechange = {
+      foreground = "#88c0d0";
+    };
+    ignored = {
+      foreground = "#4c566a";
+    };
+    conflicted = {
+      foreground = "#d08770";
+    };
+  };
+
+  git_repo = {
+    branch_main = {
+      foreground = "#4c566a";
+    };
+    branch_other = {
+      foreground = "#8fbcbb";
+    };
+    git_clean = {
+      foreground = "#3b4252";
+    };
+    git_dirty = {
+      foreground = "#b48ead";
+    };
+  };
+
+  security_context = {
+    colon = {
+      foreground = "#4c566a";
+    };
+    user = {
+      foreground = "#4c566a";
+    };
+    role = {
+      foreground = "#88c0d0";
+    };
+    typ = {
+      foreground = "#5e81ac";
+    };
+    range = {
+      foreground = "#b48ead";
+    };
+  };
+
+  file_type = {
+    image = {
+      foreground = "#8fbcbb";
+    };
+    video = {
+      foreground = "#88c0d0";
+    };
+    music = {
+      foreground = "#8fbcbb";
+    };
+    lossless = {
+      foreground = "#81a1c1";
+    };
+    crypto = {
+      foreground = "#bf616a";
+    };
+    document = {
+      foreground = "#d8dee9";
+    };
+    compressed = {
+      foreground = "#d08770";
+    };
+    temp = {
+      foreground = "#4c566a";
+    };
+    compiled = {
+      foreground = "#4c566a";
+    };
+    build = {
+      foreground = "#a3be8c";
+    };
+    source = {
+      foreground = "#b48ead";
+    };
+  };
+
+  punctuation = {
+    foreground = "#4c566a";
+  };
+  date = {
+    foreground = "#ebcb8b";
+  };
+  inode = {
+    foreground = "#4c566a";
+  };
+  blocks = {
+    foreground = "#4c566a";
+  };
+  header = {
+    foreground = "#d8dee9";
+  };
+  octal = {
+    foreground = "#d08770";
+  };
+  flags = {
+    foreground = "#b48ead";
+  };
+
+  symlink_path = {
+    foreground = "#8fbcbb";
+  };
+  control_char = {
+    foreground = "#d08770";
+  };
+  broken_symlink = {
+    foreground = "#bf616a";
+  };
+  broken_path_overlay = {
+    foreground = "#bf616a";
+  };
+}
