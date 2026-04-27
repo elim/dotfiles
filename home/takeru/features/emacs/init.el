7708164183f9d03c2a79ff4867dc8e02081048d7
Takeru Naito <takeru.naito@gmail.com>
2026-04-27 19:36:12 +0900

    refactor(emacs): replace top-level language wrapper leaves with section comments
    The *programming-languages and *writing-and-markup wrapper leaves are
    dissolved: their children become top-level leaves and the grouping is
    expressed as section comment headings instead.
    
    No settings were changed; only the structural nesting changed.

--- home/takeru/features/emacs/init.el
+++ home/takeru/features/emacs/init.el
@@ -792,119 +792,132 @@ When called with a prefix argument (C-u), prompt for input in the minibuffer."
 
 ;;; Programming languages
 
-(leaf *programming-languages
+(leaf cc-mode
+  :defun c-toggle-auto-hungry-state
+  :preface
+  (defun elim:c-mode-common-hook-func ()
+    (c-set-style "bsd")
+    (set-variable 'indent-tabs-mode nil)
+    (set-variable 'c-basic-offset 2)
+    (c-toggle-auto-hungry-state -1)
+    (subword-mode 1))
+  :hook ((c-mode-common-hook . elim:c-mode-common-hook-func)))
+
+(leaf elisp-mode
+  :hook (emacs-lisp-mode-hook . elim:emacs-lisp-mode-hook-func)
   :config
-  (leaf cc-mode
-    :defun c-toggle-auto-hungry-state
-    :preface
-    (defun elim:c-mode-common-hook-func ()
-      (c-set-style "bsd")
-      (set-variable 'indent-tabs-mode nil)
-      (set-variable 'c-basic-offset 2)
-      (c-toggle-auto-hungry-state -1)
-      (subword-mode 1))
-    :hook ((c-mode-common-hook . elim:c-mode-common-hook-func)))
-  (leaf elisp-mode
-    :hook (emacs-lisp-mode-hook . elim:emacs-lisp-mode-hook-func)
-    :config
-    (defun elim:emacs-lisp-mode-hook-func ()
-      (set-variable 'indent-tabs-mode nil)
-      (hs-minor-mode +1)))
-  (leaf elisp-slime-nav
-    :hook ((emacs-lisp-mode-hook
-            lisp-interaction-mode-hook
-            ielm-mode-hook) .  elisp-slime-nav-mode))
-  (leaf go-mode
-    :preface
-    (defun elim:go-mode-hook-func ()
-      (set (make-local-variable 'tab-width) 4))
-    :hook (go-mode-hook . elim:go-mode-hook-func))
-  (leaf lua-ts-mode
-    :mode (("\\.lua\\'" . lua-ts-mode))
-    :custom ((lua-ts-mode-indent-offset . 2)))
-  (leaf js
-    :custom ((js-indent-level . 2)))
-  (leaf *ruby
-    :config
-    (leaf rubocop)
-    (leaf ruby-end)
-    (leaf ruby-mode
-      :bind (:ruby-mode-map
-             ("C-m" . reindent-then-newline-and-indent))
-      :custom ((ruby-deep-indent-paren-style . nil)
-               (ruby-flymake-use-rubocop-if-available . nil)
-               (ruby-insert-encoding-magic-comment . nil)))
-    (leaf rspec-mode))
-  (leaf *typescript-javascript
-    :doc "Modern TypeScript/JavaScript development with tree-sitter and LSP"
-    :config
-    (leaf typescript-ts-mode
-      :mode (("\\.ts\\'" . typescript-ts-mode)
-             ("\\.tsx\\'" . tsx-ts-mode)
-             ("\\.js\\'" . js-ts-mode)
-             ("\\.jsx\\'" . jsx-ts-mode))
-      :custom ((typescript-ts-mode-indent-offset . 2)
-               (js-ts-mode-indent-offset . 2))
-      :hook ((typescript-ts-mode-hook tsx-ts-mode-hook js-ts-mode-hook) . eglot-ensure))
-    (leaf add-node-modules-path
-      :doc "npm bin is removed in npm v9"
-      :url "https://github.com/codesuki/add-node-modules-path/issues/23"
-      :custom (add-node-modules-path-command . '("echo \"$(npm root)/.bin\""))
-      :hook ((typescript-ts-mode-hook js-ts-mode-hook) . add-node-modules-path)))
-  (leaf tsx-ts-mode :mode "\\.tsx\\'"))
+  (defun elim:emacs-lisp-mode-hook-func ()
+    (set-variable 'indent-tabs-mode nil)
+    (hs-minor-mode +1)))
+
+(leaf elisp-slime-nav
+  :hook ((emacs-lisp-mode-hook
+          lisp-interaction-mode-hook
+          ielm-mode-hook) .  elisp-slime-nav-mode))
+
+(leaf go-mode
+  :preface
+  (defun elim:go-mode-hook-func ()
+    (set (make-local-variable 'tab-width) 4))
+  :hook (go-mode-hook . elim:go-mode-hook-func))
+
+(leaf lua-ts-mode
+  :mode (("\\.lua\\'" . lua-ts-mode))
+  :custom ((lua-ts-mode-indent-offset . 2)))
+
+(leaf js
+  :custom ((js-indent-level . 2)))
+
+(leaf *ruby
+  :config
+  (leaf rubocop)
+  (leaf ruby-end)
+  (leaf ruby-mode
+    :bind (:ruby-mode-map
+           ("C-m" . reindent-then-newline-and-indent))
+    :custom ((ruby-deep-indent-paren-style . nil)
+             (ruby-flymake-use-rubocop-if-available . nil)
+             (ruby-insert-encoding-magic-comment . nil)))
+  (leaf rspec-mode))
+
+(leaf *typescript-javascript
+  :doc "Modern TypeScript/JavaScript development with tree-sitter and LSP"
+  :config
+  (leaf typescript-ts-mode
+    :mode (("\\.ts\\'" . typescript-ts-mode)
+           ("\\.tsx\\'" . tsx-ts-mode)
+           ("\\.js\\'" . js-ts-mode)
+           ("\\.jsx\\'" . jsx-ts-mode))
+    :custom ((typescript-ts-mode-indent-offset . 2)
+             (js-ts-mode-indent-offset . 2))
+    :hook ((typescript-ts-mode-hook tsx-ts-mode-hook js-ts-mode-hook) . eglot-ensure))
+  (leaf add-node-modules-path
+    :doc "npm bin is removed in npm v9"
+    :url "https://github.com/codesuki/add-node-modules-path/issues/23"
+    :custom (add-node-modules-path-command . '("echo \"$(npm root)/.bin\""))
+    :hook ((typescript-ts-mode-hook js-ts-mode-hook) . add-node-modules-path)))
+
+(leaf tsx-ts-mode :mode "\\.tsx\\'")
 
 ;;; Configuration and data
 
-(leaf *configuration-and-data
-  :config
-  (leaf dockerfile-mode)
-  (leaf json-mode)
-  (leaf nix-mode
-    :doc "Major mode for editing .nix files"
-    :req "emacs-25.1" "magit-section-0" "transient-0.3"
-    :tag "unix" "tools" "languages" "nix" "emacs>=25.1"
-    :url "https://github.com/NixOS/nix-mode"
-    :added "2023-03-28"
-    :emacs>= 25.1
-    :after magit-section)
-  (leaf sh-script
-    :mode ("\\.env\\'" "\\.env.sample\\'")
-    :custom ((sh-basic-offset . 2)
-             (sh-indentation . 2)))
-  (leaf terraform-mode)
-  (leaf yaml-mode))
+(leaf dockerfile-mode)
+
+(leaf json-mode)
+
+(leaf nix-mode
+  :doc "Major mode for editing .nix files"
+  :req "emacs-25.1" "magit-section-0" "transient-0.3"
+  :tag "unix" "tools" "languages" "nix" "emacs>=25.1"
+  :url "https://github.com/NixOS/nix-mode"
+  :added "2023-03-28"
+  :emacs>= 25.1
+  :after magit-section)
+
+(leaf sh-script
+  :mode ("\\.env\\'" "\\.env.sample\\'")
+  :custom ((sh-basic-offset . 2)
+           (sh-indentation . 2)))
+
+(leaf terraform-mode)
+
+(leaf yaml-mode)
 
 ;;; Writing and markup
 
-(leaf *writing-and-markup
-  :config
-  (leaf css-mode
-    :custom ((css-indent-offset . 2)))
-  (leaf feature-mode
-    :after org org-table)
-  (leaf html-ts-mode :mode "\\.html?\\'")
-  (leaf markdown-mode
-    :mode (("\\.md\\'" "\\ISSUE_EDITMSG\\'") . gfm-mode)
-    :bind (:markdown-mode-map
-           ("<S-tab>" . markdown-shifttab)
-           ("C-c 1"   . markdown-insert-header-atx-1)
-           ("C-c 2"   . markdown-insert-header-atx-2)
-           ("C-c b"   . markdown-insert-bold)
-           ("C-c i"   . markdown-insert-italic))
-    :custom
-    ((markdown-asymmetric-header            . t)
-     (markdown-fontify-code-blocks-natively . t)
-     (markdown-gfm-use-electric-backquote   . nil)
-     (markdown-header-scaling               . nil)
-     (markdown-hr-strings                   . '("* * *\n\n"))
-     (markdown-marginalize-headers          . nil)))
-  (leaf org :require org org-table)
-  (leaf slim-mode)
-  (leaf text-mode
-    :preface
-    (defun elim:text-mode-hook-func ()
-      (set-variable 'indent-tabs-mode nil))
-    :hook (text-mode-hook . elim:text-mode-hook-func)))
+(leaf css-mode
+  :custom ((css-indent-offset . 2)))
+
+(leaf feature-mode
+  :after org org-table)
+
+(leaf html-ts-mode :mode "\\.html?\\'")
+
+(leaf markdown-mode
+  :mode (("\\.md\\'" "\\ISSUE_EDITMSG\\'") . gfm-mode)
+  :bind (:markdown-mode-map
+         ("<S-tab>" . markdown-shifttab)
+         ("C-c 1"   . markdown-insert-header-atx-1)
+         ("C-c 2"   . markdown-insert-header-atx-2)
+         ("C-c b"   . markdown-insert-bold)
+         ("C-c i"   . markdown-insert-italic))
+  :custom
+  ((markdown-asymmetric-header            . t)
+   (markdown-fontify-code-blocks-natively . t)
+   (markdown-gfm-use-electric-backquote   . nil)
+   (markdown-header-scaling               . nil)
+   (markdown-hr-strings                   . '("* * *\n\n"))
+   (markdown-marginalize-headers          . nil)))
+
+(leaf org :require org org-table)
+
+(leaf slim-mode)
+
+(leaf text-mode
+  :preface
+  (defun elim:text-mode-hook-func ()
+    (set-variable 'indent-tabs-mode nil))
+  :hook (text-mode-hook . elim:text-mode-hook-func))
 
 (provide 'init)
 
