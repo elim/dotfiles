681fa621c67a4364edc1e6de7a9747267c56d4ae
Takeru Naito <takeru.naito@gmail.com>
2026-04-27 15:31:22 +0900

    refactor(emacs): move dialog box setting into *interfaces
    use-dialog-box controls UI interaction and belongs in *interfaces
    rather than *environments.
    
    No behavior was changed.

--- home/takeru/features/emacs/init.el
+++ home/takeru/features/emacs/init.el
@@ -441,7 +441,6 @@
 
 (leaf *environments
   :custom `((gc-cons-threshold . ,(* 128 1024 1024))
-            (use-dialog-box . nil)
             (user-mail-address . "takeru.naito@gmail.com")
             (user-full-name . "Takeru Naito"))
   :config
@@ -459,6 +458,7 @@
            (select-active-regions . nil)
            (show-trailing-whitespace . nil)
            (truncate-lines . nil)
+           (use-dialog-box . nil)
            (visible-bell . t))
   :config
   (put 'dired-find-alternate-file 'disabled nil)
