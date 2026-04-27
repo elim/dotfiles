cfa1f8d61ad3f8764e5d4a273544b452d4765b18
Takeru Naito <takeru.naito@gmail.com>
2026-04-27 15:32:31 +0900

    refactor(emacs): move yes-or-no prompt behavior into *interfaces
    The y-or-n-p alias controls interactive prompt style and fits
    *interfaces better than *environments.
    
    No behavior was changed.

--- home/takeru/features/emacs/init.el
+++ home/takeru/features/emacs/init.el
@@ -441,9 +441,7 @@
 
 (leaf *environments
   :custom `((user-mail-address . "takeru.naito@gmail.com")
-            (user-full-name . "Takeru Naito"))
-  :config
-  (defalias 'yes-or-no-p 'y-or-n-p))
+            (user-full-name . "Takeru Naito")))
 
 (leaf *interfaces
   :custom ((frame-title-format . `(" %b " (buffer-file-name "( %f )")))
@@ -457,6 +455,7 @@
            (use-dialog-box . nil)
            (visible-bell . t))
   :config
+  (defalias 'yes-or-no-p 'y-or-n-p)
   (put 'dired-find-alternate-file 'disabled nil)
   (put 'narrow-to-region 'disabled nil)
   (put 'set-goal-column 'disabled nil)
